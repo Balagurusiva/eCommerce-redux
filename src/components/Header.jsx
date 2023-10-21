038dd98f0bca52985718998a5ed0ad1d6b198e22
Balaguru <balagurusiva2002@gmail.com>
2023-10-21 18:24:17 +0530

    modified:   src/components/Header.jsx 	modified:   src/components/ProductComponent.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='w-full  bg-indigo-950 h-12 p-2 fixed'>
+    <div className='w-full z-10  bg-indigo-950 h-12 p-2 fixed'>
         <p  className='font-bold text-2xl text-white flex items-center h-full'>shop</p> 
     </div>
   )
--- src/components/ProductComponent.jsx
+++ src/components/ProductComponent.jsx
@@ -9,24 +9,24 @@ const ProductComponent = () => {
         const { id, title, image, price, category } = product
         return (
 
-            <Link to={'product/' + id}><div className=" flex justify-around flex-row wrap items-center p-[20px]  mt-20  w-[300px]  max-h-39 border-2 border-sky-500 rounded-xl" key={id}>
-
-                <div className="flex justify- items-center flex-col">
-                    <div className="image min-h-[200px] max-h-[200px] min-w-[150px]  max--[50px] ">
-                          <img src={image} alt={title} className=' h-[180px]  object-contain' />
-                    </div>
-                    <div className="content">
-                        <div className="header">{title}</div>
-                        <div className="meta price">$ {price}</div>
-                        <div className="meta">{category}</div>
+            <Link to={'product/' + id} className=' card  flex  max-h-[350px]  border-2 border-sky-500 flex-col flex  flex-col wrap items-center p-[20px]     w-[300px]    rounded-xl  '>
+                <div key={id}>
+                    <div className="flex   items-center flex-col">
+                        <div className="image min-h-[200px] max-h-[200px] min-w-[150px]  max--[50px] ">
+                            <img src={image} alt={title} className=' h-[180px]  object-contain' />
+                        </div>
                     </div>
                 </div>
-
-            </div></Link>
-             )
+                <div className="content">
+                            <div className="header">{title}</div>
+                            <div className="meta price">$ {price}</div>
+                            <div className="meta">{category}</div>
+                </div>
+            </Link>
+        )
     })
     return (
-        <div className='w-full  flex justify-evenly items-center flex-col md:flex-row gap-2 flex-wrap  '>{renderList}</div>
+        <div className='w-full absolute top-[70px] z-1 flex justify-evenly items-center flex-col md:flex-row gap-6 flex-wrap   '>{renderList}</div>
     )
 }
 
